e0aeae96238273f14a6965f9a46e85678b11ed73
chetstar <cmeinzer@acbhcs.org>
2014-11-02 22:53:33 -0800

    structure in place

--- flask/bin/activate.fish
+++ flask/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/chet/Desktop/kiriko/python-getting-started/flask"
+set -gx VIRTUAL_ENV "/home/chet/Desktop/kiriko/flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
