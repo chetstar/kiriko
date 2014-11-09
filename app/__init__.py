082255fa7ce0bec6c83d85e75ddd66a3c15e5305
chetstar <cmeinzer@acbhcs.org>
2014-11-08 17:17:16 -0800

    bootstraped

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,18 @@
 from flask import Flask
-
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+Bootstrap(app)
 from app import views
+
+import os
+from flask import Flask
+from flask_bootstrap import Bootstrap
+app = Flask(__name__)
+
+Bootstrap(app)
+
+from views import *
+
+if __name__ == "__main__":
+ port = int(os.environ.get("PORT", 5000))
+ app.run(host='0.0.0.0', port=port)
